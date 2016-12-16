a0373a8d691563c44ca05bd0425d48a7a5a59f77
Andrej Kabachnik <andrej_ka@gmx.de>
2016-12-16 17:03:06 +0100

    NEW model source table for error codes

---
+++ ModelSource/updates/0005.add_error_code_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `exf_error` (
+  `oid` binary(16) NOT NULL,
+  `app_oid` binary(16) NOT NULL,
+  `error_code` varchar(8) NOT NULL,
+  `error_text` varchar(250) DEFAULT NULL,
+  `created_on` datetime NOT NULL,
+  `modified_on` datetime NOT NULL,
+  `created_by_user_oid` binary(16) DEFAULT NULL,
+  `modified_by_user_oid` binary(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE exf_error_code;
+
+DROP TABLE exf_config;
