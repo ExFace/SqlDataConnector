88656794fceaca76a59117fad6a90a7c830288a4
Andrej Kabachnik <andrej_ka@gmx.de>
2017-01-11 10:43:08 +0100

    FIX added more db indexes

--- ModelSource/updates/0006.new_db_indexes.sql
+++ ModelSource/updates/0006.new_db_indexes.sql
@@ -5,6 +5,7 @@ DELETE FROM `exf_object` WHERE oid = 0x38300000000000000000000000000000;
 ALTER TABLE `exf_object` DROP INDEX `alias`, ADD INDEX `alias` (`object_alias`) USING BTREE;
 ALTER TABLE `exf_object` DROP INDEX `app_oid`, ADD INDEX `app_oid` (`app_oid`) USING BTREE;
 ALTER TABLE `exf_object` DROP INDEX `parent_object_oid`, ADD INDEX `parent_object_oid` (`parent_object_oid`) USING BTREE;
+ALTER TABLE `exf_object` DROP INDEX `data_source_oid`, ADD INDEX `data_source_oid` (`data_source_oid`) USING BTREE;
 ALTER TABLE `exf_object` DROP INDEX `alias+app_oid`, ADD UNIQUE `alias+app_oid` (`object_alias`, `app_oid`) USING BTREE;
 
 /* Add indexes to attribute table*/
