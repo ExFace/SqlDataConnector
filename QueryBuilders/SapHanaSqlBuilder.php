f5a98e4f41e2dd513c9957d88f32b19c068402bb
Andrej Kabachnik <andrej_ka@gmx.de>
2017-04-07 11:01:01 +0200

    FIX group by with custom SQL statements in SAP HANA

--- QueryBuilders/SapHanaSqlBuilder.php
+++ QueryBuilders/SapHanaSqlBuilder.php
@@ -10,4 +10,22 @@
  */
 class SapHanaSqlBuilder extends MySQL {
 	
+	/**
+	 * SAP HANA supports custom SQL statements in the GROUP BY clause. The rest is similar to MySQL
+	 *  
+	 * {@inheritDoc}
+	 * @see \exface\SqlDataConnector\QueryBuilders\AbstractSQL::build_sql_group_by()
+	 */
+	protected function build_sql_group_by(\exface\Core\CommonLogic\QueryBuilder\QueryPart $qpart, $select_from = null){
+		$output = '';
+		if ($this->check_for_sql_statement($qpart->get_attribute()->get_data_address())){
+			if (is_null($select_from)){
+				$select_from = $qpart->get_attribute()->get_relation_path()->to_string() ? $qpart->get_attribute()->get_relation_path()->to_string() : $this->get_main_object()->get_alias();
+			}
+			$output = str_replace('[#alias#]', $select_from, $qpart->get_attribute()->get_data_address());
+		} else {
+			$output = parent::build_sql_group_by($qpart, $select_from);
+		}
+		return $output;
+	}
 }
